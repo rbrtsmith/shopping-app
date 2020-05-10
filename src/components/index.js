6d7d79587d664ad3e9300d55f6479fff81132d69
Robert Smith <rbrtsmith84@googlemail.com>
2020-05-10 18:09:57 +0100

    fix build

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
 export { setupFirebase, FirebaseContext } from './Firebase'
 export { Navigation } from './Navigation'
-export { AddNewList } from './AddNewList' 
 export { StyledListItem, StyledListItemSelectButton } from './ListItem'
